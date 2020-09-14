ec40febbde1bb893c2a2d68f0855d0ff9b470845
Fanoyrios10 <54408484+Fanoyrios10@users.noreply.github.com>
2020-09-15 01:38:52 +0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -12,18 +12,69 @@ for (i = 0; i < dropdown.length; i++) {
   }
   });
 }
+
 function playMusic(){
-  var music = new Audio('audio/Wimdows Sounds/Windows 98/Windows 98 menu popup.mp3');
+  var music = new Audio('../../../audio/Wimdows Sounds/Windows 98/Windows 98 menu popup.mp3');
   music.play();
   }
-  function playMusic2(){
-    var music = new Audio('audio/Wimdows Sounds/Windows 98/Windows 98 menu command.mp3');
-    music.play();
-    }
-    function playMusic1974(){
-      var music = new Audio('audio/Wimdows Sounds/Windows 98/Windows 98 menu command.mp3');
-      music.play();
-      setTimeout(function () {
-        location.href = "Home-page-gr.html";
-    }, 1000);
-      }
+
+function playMusic2(){
+  var music = new Audio('../../../audio/Wimdows Sounds/Windows 98/Windows 98 menu command.mp3');
+  music.play();
+  }
+
+function playMusic1974(){
+  var music = new Audio('../../../audio/Wimdows Sounds/Windows 98/Windows 98 menu command.mp3');
+  music.play();
+  setTimeout(function () {
+    location.href = "Home-page-gr.html";
+    }, 500);
+  }
+
+function playMusicWindowsDR5(){
+  var music = new Audio('../../../audio/Wimdows Sounds/Windows 98/Windows 98 menu command.mp3');
+  music.play();
+  setTimeout(function () {
+    location.href = "Windows-1.0-DR5-gr.html";
+    }, 500);
+  }
+
+function playMusicWindowsPR(){
+  var music = new Audio('../../../audio/Wimdows Sounds/Windows 98/Windows 98 menu command.mp3');
+  music.play();
+  setTimeout(function () {
+    location.href = "Windows-Premiere-Edition-gr.html";
+    }, 500);
+  }
+
+function playMusicWindows104(){
+  var music = new Audio('../../../audio/Wimdows Sounds/Windows 98/Windows 98 menu command.mp3');
+  music.play();
+  setTimeout(function () {
+    location.href = "Windows-1.04-gr.html";
+    }, 500);
+}
+
+function playMusicPCem(){
+  var music = new Audio('../../../audio/Wimdows Sounds/Windows 98/Windows 98 menu command.mp3');
+  music.play();
+  setTimeout(function () {
+    location.href = "PCem-Setup-gr.html";
+    }, 500);
+}
+
+function playMusicUnderConstruction(){
+  var music = new Audio('../../../audio/Wimdows Sounds/Windows 98/Windows 98 menu command.mp3');
+  music.play();
+  setTimeout(function () {
+    location.href = "../../under_construction.html";
+    }, 500);
+}
+
+function playMusicIndex(){
+  var music = new Audio('../../../audio/Wimdows Sounds/Windows 98/Windows 98 menu command.mp3');
+  music.play();
+  setTimeout(function () {
+    location.href = "../../../index.html";
+    }, 500);
+}
