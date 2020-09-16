f383d132587e66d11f648f0817ecd75c4756929d
Fanoyrios10 <54408484+Fanoyrios10@users.noreply.github.com>
2020-09-16 20:33:14 +0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -78,3 +78,56 @@ function playMusicIndex(){
     location.href = "../../../index.html";
     }, 500);
 }
+
+var mybutton = document.getElementById("GoToTop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function background1() {
+  var x = document.getElementById("Texture");
+  x.style.backgroundImage = "url(../../../images/Background_Tiles/5003.gif)";
+  x.style.color = "black";
+}
+
+function background2() {
+  var x = document.getElementById("Texture");
+  x.style.backgroundImage = "url(../../../images/Background_Tiles/Black.gif)";
+  x.style.color = "white";
+}
+
+function background3() {
+  var x = document.getElementById("Texture");
+  x.style.backgroundImage = "url(../../../images/Background_Tiles/Blue.gif)";
+  x.style.color = "Black";
+}
+
+function background4() {
+  var x = document.getElementById("Texture");
+  x.style.backgroundImage = "url(../../../images/Background_Tiles/YellowLight.gif)";
+  x.style.color = "Black";
+}
+
+function background5() {
+  var x = document.getElementById("Texture");
+  x.style.backgroundImage = "url(../../../images/Background_Tiles/wrinkled-paper-texture_1100-12.gif)";
+  x.style.color = "Black";
+}
+
+function background6() {
+  var x = document.getElementById("Texture");
+  x.style.backgroundImage = "url(../../../images/Background_Tiles/orange_paper.gif)";
+  x.style.color = "Black";
+}
